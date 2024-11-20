6e73dcffb4b260051368d711778cd1105d5074fc
Rafael Asmoucha <rasmoucha@gmail.com>
2024-11-20 12:15:33 +0200

    added functionality for negative inputs on algebra.java

--- Algebra.java
+++ Algebra.java
@@ -6,14 +6,14 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
+	    
+		System.out.println(plus(2,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
  		System.out.println(times(3,4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
-		System.out.println("divide here:");
    		System.out.println(div(12,3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
@@ -22,14 +22,49 @@ public class Algebra {
    		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
+		
 		// note for reader: should be rather easy to understand so i have not left unnecessary comments all over my (lovely) code
-	}  
+	}
+
+	// returns |a| for a
+	public static int abs(int a) {
+		int result = a;
+		if (a < 0) {
+			result = 0;
+			int counter = 0;
+			while (counter > a) {
+				result++;
+				counter--;
+			}
+		}
+		return result;
+	}
+
+	// returns -|a| for a
+	public static int negative_abs(int a) {
+		int result = a;
+		if (a > 0) {
+			result = 0;
+			int counter = 0;
+			while (counter < a) {
+				result--;
+				counter++;
+			}
+		}
+		return result;
+	}
+
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		int result = x1;
-		for (int i = 0; i < x2; i++) {
-			result++;
+		for (int i = 0; i < abs(x2); i++) {
+			if (x2 > 0) {
+				result++;
+			}
+			else {
+				result--;
+			}
 		}
 		return result;
 	}
@@ -37,8 +72,13 @@ public class Algebra {
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		int result = x1;
-		for (int i = 0; i < x2; i++) {
-			result--;
+		for (int i = 0; i < abs(x2); i++) {
+			if (x2 > 0) {
+				result--;
+			}
+			else {
+				result++;
+			}
 		}
 		return result;
 	}
@@ -46,8 +86,13 @@ public class Algebra {
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		int result = 0;
-		for (int i = 0; i < x2; i++) {
-			result = plus(result, x1);
+		for (int i = 0; i < abs(x2); i++) {
+			if (x2 > 0) {
+				result = plus(result, x1);
+			}
+			else {
+				result = minus(result, x1);
+			}
 		}
 		return result;
 	}
@@ -65,11 +110,14 @@ public class Algebra {
 	public static int div(int x1, int x2) {
 		int result = 1;
 		while (true) {
-			if (times(result, x2) > x1) {
+			if (abs(times(result, x2)) > abs(x1)) {
 				result--;
 				break;
 			}
 			result++;
+		}
+		if (times(x1,x2) < 0 ) {
+			result = negative_abs(result);
 		}
 		return result;
 	}
@@ -77,9 +125,17 @@ public class Algebra {
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
 		int result = x1;
-		while (result >= x2) {
-			result = minus(result, x2);
+		if (x1 > 0) {
+			while (result >= x2) {
+				result = minus(result, x2);
+			}
 		}
+		else {
+			while (result < 0) {
+				result = plus(result, x2);
+			}
+		}
+		
 		return result;
 	}	
 
