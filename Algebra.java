9e4d32a5ff8abbc2d1e9d9fec9d378bcf107ff5d
Rafael Asmoucha <rasmoucha@gmail.com>
2024-11-21 10:38:54 +0200

    fixed merge conflicts i think

--- Algebra.java
+++ Algebra.java
@@ -23,45 +23,129 @@ public class Algebra {
    		System.out.println(sqrt(76123));
 	}  
 
+	// returns |a| for a
+	public static int abs(int a) {
+		int result = a;
+		if (a < 0) {
+			result = 0;
+			int counter = 0;
+			while (counter > a) {
+				result++;
+				counter--;
+			}
+		}
+		return result;
+	}
+
+	// returns -|a| for a
+	public static int negative_abs(int a) {
+		int result = a;
+		if (a > 0) {
+			result = 0;
+			int counter = 0;
+			while (counter < a) {
+				result--;
+				counter++;
+			}
+		}
+		return result;
+	}
+
+
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		for (int i = 0; i < abs(x2); i++) {
+			if (x2 > 0) {
+				result++;
+			}
+			else {
+				result--;
+			}
+		}
+		return result;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		for (int i = 0; i < abs(x2); i++) {
+			if (x2 > 0) {
+				result--;
+			}
+			else {
+				result++;
+			}
+		}
+		return result;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0;
+		for (int i = 0; i < abs(x2); i++) {
+			if (x2 > 0) {
+				result = plus(result, x1);
+			}
+			else {
+				result = minus(result, x1);
+			}
+		}
+		return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 1;
+		for (int i = 0; i < n; i++) {
+			result = times(result, x);
+		}
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 1;
+		while (true) {
+			if (abs(times(result, x2)) > abs(x1)) {
+				result--;
+				break;
+			}
+			result++;
+		}
+		if (times(x1,x2) < 0 ) {
+			result = negative_abs(result);
+		}
+		return result;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		if (x1 > 0) {
+			while (result >= x2) {
+				result = minus(result, x2);
+			}
+		}
+		else {
+			while (result < 0) {
+				result = plus(result, x2);
+			}
+		}
+		
+		return result;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0;
+		while (true) {
+			if (times(result, result) > x) {
+				result--;
+				break;
+			}
+			result++;
+		}
+		return result;
 	}	  	  
 }
